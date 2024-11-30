daeaf2d83b6ec10c00e72c69a1aa1e7166204399
Maksim Pegov <maksim.peg@gmail.com>
2024-11-30 18:53:42 +0100

    test: jest and tests update

--- src/clients/clients.controller.spec.ts
+++ src/clients/clients.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsController } from './clients.controller';
+import { ClientsService } from './clients.service';
 
 describe('ClientsController', () => {
   let controller: ClientsController;
@@ -7,6 +8,7 @@ describe('ClientsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ClientsController],
+      providers: [ClientsService],
     }).compile();
 
     controller = module.get<ClientsController>(ClientsController);
--- src/organisations/organisations.controller.spec.ts
+++ src/organisations/organisations.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganisationsController } from './organisations.controller';
+import { OrganisationsService } from './organisations.service';
+import { ClientsService } from '../clients/clients.service';
 
 describe('OrganisationsController', () => {
   let controller: OrganisationsController;
@@ -7,6 +9,7 @@ describe('OrganisationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganisationsController],
+      providers: [OrganisationsService, ClientsService],
     }).compile();
 
     controller = module.get<OrganisationsController>(OrganisationsController);
