7c109ccd40de1bd70bedf09c684435b97abd221e
Maksim Pegov <maksim.peg@gmail.com>
2024-12-25 13:09:02 +0100

    feat: enable CORS with all origins and specified methods in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true, // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
